7f04dc3e6c31dd4c7a3a905840903539333eefb0
Navith <navithnavithnavith+git@gmail.com>
2018-10-28 01:03:22 -0400

    add tailwindcss-transition plugin

--- js/tailwind.js
+++ js/tailwind.js
@@ -905,6 +905,14 @@ module.exports = {
       // center: true,
       // padding: '1rem',
     }),
+    require('tailwindcss-transition')({
+      standard: 'all .3s ease',
+      transitions: {
+        'slow': 'all 2s ease',
+        'normal-in-out-quad': 'all 2s cubic-bezier(0.455, 0.03, 0.515, 0.955)',
+        'slow-in-out-quad': 'all 2s cubic-bezier(0.455, 0.03, 0.515, 0.955)',
+      }  
+    })
   ],
 
 
