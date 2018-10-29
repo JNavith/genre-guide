de7ee789b028610e34d8768b0a5be029132160d2
Navith <navithnavithnavith+git@gmail.com>
2018-10-29 14:46:25 -0400

    introduce text-md

--- js/tailwind.js
+++ js/tailwind.js
@@ -218,12 +218,14 @@ module.exports = {
     'xs': '.75rem',     // 12px
     'sm': '.875rem',    // 14px
     'base': '1rem',     // 16px
+    'md': '1rem',       // 16px
     'lg': '1.125rem',   // 18px
     'xl': '1.25rem',    // 20px
     '2xl': '1.5rem',    // 24px
     '3xl': '1.875rem',  // 30px
     '4xl': '2.25rem',   // 36px
     '5xl': '3rem',      // 48px
+    '6xl': '3.75rem',   // 60px
     '8xl': '4.5rem',    // 72px
   },
 
