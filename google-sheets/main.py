e96e7b79f0fb3786a6e0000ffe2d7847d83a6a52
Navith <navithnavithnavith+git@gmail.com>
2018-11-24 23:44:28 -0500

    add elementary logging, a comment, and put the main code into a `main` function in `google-sheets/main.py`

--- google-sheets/main.py
+++ google-sheets/main.py
@@ -22,20 +22,31 @@ from os import getenv
 from gspread import authorize
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ["https://www.googleapis.com/auth/drive.readonly"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name("client.secret.json", scope)
-
-client = authorize(credentials)
-
-genre_sheet = client.open_by_key(getenv("SHEET_KEY"))
-
-catalog = genre_sheet.worksheet("Main")
-
-catalog_header = catalog.row_values(1)
-all_tracks = catalog.range(2, 1, catalog.row_count, catalog.col_count)
-
-with open("./src/csv/all_tracks.csv", "w") as csv_file:
-	csv_writer = writer(csv_file, delimiter="\t")
+def main():
+	scope = ["https://www.googleapis.com/auth/drive.readonly"]
+	credentials = ServiceAccountCredentials.from_json_keyfile_name("client.secret.json", scope)
 	
-	for row_number, row in groupby(all_tracks, key=attrgetter("row")):
-		csv_writer.writerow(map(attrgetter("value"), row))
+	client = authorize(credentials)
+	
+	genre_sheet = client.open_by_key(getenv("SHEET_KEY"))
+	
+	catalog = genre_sheet.worksheet("Main")
+	
+	# Includes the header row because I suspect this will be helpful later on
+	all_tracks_with_header = catalog.range(1, 1, catalog.row_count, catalog.col_count)
+	
+	with open("./src/csv/all_tracks.csv", "w") as csv_file:
+		csv_writer = writer(csv_file, delimiter="\t")
+		
+		for row_number, row in groupby(all_tracks_with_header, key=attrgetter("row")):
+			csv_writer.writerow(map(attrgetter("value"), row))
+			
+			if row_number == 1:
+				print("wrote out the 1st track")
+			if row_number % 200 == 0:
+				print(f"wrote out the {row_number}th track")
+		
+		print(f"all {row_number} tracks have been written to CSV")
+
+if __name__ == "__main__":
+	main()
