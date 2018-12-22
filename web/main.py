cd8a6b31e387a1e50c70a51b1ef671e34c67971f
Navith <navithnavithnavith+git@gmail.com>
2018-12-22 14:58:29 -0500

    humble beginnings for catalog view (including song-missing-art.svg template for songs that don't have art --- aka all of them since there is no third-party integration to get artworks (yet))

--- web/main.py
+++ web/main.py
@@ -7,13 +7,34 @@ from uvicorn import run
 app = Starlette(template_directory="templates")
 
 
-@app.route('/genre')
-async def homepage(request: Request):
+@app.route("/catalog")
+@app.route("/")
+async def view_catalog(request: Request):
+	template: Template = app.get_template("views/catalog.html")
+	return HTMLResponse(template.render(request=request))
+
+
+@app.route("/genre")
+async def view_genre(request: Request):
 	if request.query_params["name"] == "Space Bass":
-		template: Template = app.get_template("view-genre.html")
+		template: Template = app.get_template("views/genre.html")
 		return HTMLResponse(template.render(request=request))
 	
 	return Response("whoops", status_code=404)
+
+
+@app.route("/svg/song-missing-art.svg")
+async def song_missing_art(request: Request):
+	# todo: proper solution (probably over graphql)
+	lookup = {
+		"Space Bass": ("#404040", "#ffffff")
+	}
+	
+	template: Template = app.get_template("components/song-missing-art.svg")
+	
+	background_color, fill_color = lookup.get(request.query_params["genre"], (None, None))
+	
+	return Response(template.render(request=request, background_color=background_color, fill_color=fill_color), media_type="image/svg+xml")
 
 
 if __name__ == '__main__':
