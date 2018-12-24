e82448bc166de300e7961e3ae6660a609dd2c952
Navith <navithnavithnavith+git@gmail.com>
2018-12-24 15:47:14 -0500

    fix color resolving behavior

--- graphql-server/main.py
+++ graphql-server/main.py
@@ -18,37 +18,46 @@ loop = uvloop_setup()
 class Color(ObjectType):
 	name = String(required=True)
 	hex = String(required=True)
+	
+	@staticmethod
+	async def _get_hex_color(genre_name: str) -> Optional[str]:
+		"""Returns None for subgenres, and a hex code like "#00aaff" for genres"""
+		return loads((await redis.hget(Subgenre._get_redis_key(genre_name), "color")).decode("utf8"))
+	
+	async def resolve_hex(self, info):
+		return await Color._get_hex_color(self.name)
 
 
 class Subgenre(ObjectType):
 	name = String(required=True)
 	is_genre = Boolean(required=True)
-	genre = Field(lambda: Subgenre)
+	genre = Field(lambda: Subgenre, required=True)
 	color = Field(Color)
 	origins = List(lambda: Subgenre, required=True)
 	subgenres = List(lambda: Subgenre, required=True)
 	
+	@staticmethod
+	def _get_redis_key(subgenre_name: str) -> str:
+		return f"subgenre:{subgenre_name}"
+	
 	@property
 	def _redis_key(self):
-		return f"subgenre:{self.name}"
+		return Subgenre._get_redis_key(self.name)
 	
 	async def resolve_is_genre(self, info):
 		result: Optional[str] = await redis.hget(self._redis_key, "is_genre")
 		
+		# Implied returning None for None (though that shouldn't happen?)
 		if result is not None:
 			return loads(result)
 	
 	async def resolve_genre(self, info):
 		return Subgenre(name=(await redis.hget(self._redis_key, "genre")).decode("utf8"))
 	
-	async def _get_hex_color(self):
-		return loads((await redis.hget(self._redis_key, "color")).decode("utf8"))
-	
 	async def resolve_color(self, info):
 		genre = await self.resolve_genre(info)
 		name = genre.name
-		hex = await genre._get_hex_color()
-		return Color(name=name, hex=hex)
+		return Color(name=name)
 	
 	async def resolve_origins(self, info):
 		return [Subgenre(name=name) for name in loads(await redis.hget(self._redis_key, "origins"))]
@@ -71,7 +80,7 @@ class Query(ObjectType):
 app.add_route('/graphql', GraphQLApp(schema=Schema(query=Query, auto_camelcase=False), executor=AsyncioExecutor(loop=loop)))
 
 if __name__ == '__main__':
-	# Yes, we are really using a global redis connection for all queries
+	# Yes, we are really using a global redis connection for all queries, without authentication or rate-limiting
 	redis: Redis = loop.run_until_complete(create_redis_pool("redis://redis", password=getenv("REDIS_PASSWORD")))
 	with closing(redis):
 		run(app, host='0.0.0.0', port=80, loop=loop)
