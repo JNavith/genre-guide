cfddc2012d96e7b606d9edea6054386969256592
Navith <navithnavithnavith+git@gmail.com>
2018-12-25 00:45:46 -0500

    implement grouping of Redis commands (for whatever reason -- premature optimization) when querying the GraphQL server, and allow the ability to query an individual subgenre

--- graphql-server/main.py
+++ graphql-server/main.py
@@ -13,10 +13,11 @@
 #    
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program. If not, see <https://www.gnu.org/licenses/>.
-
+from asyncio import sleep
 from contextlib import closing
 from json import loads
 from os import getenv
+from sys import stderr
 from typing import Optional
 
 from aioredis import Redis, create_redis_pool
@@ -32,19 +33,23 @@ loop = uvloop_setup()
 
 
 class Color(ObjectType):
+	"""A color for genres and subgenres. Includes information about the name (which genre it's tied to) and a hex representation."""
+	
 	name = String(required=True)
 	hex = String(required=True)
 	
 	@staticmethod
 	async def _get_hex_color(genre_name: str) -> Optional[str]:
 		"""Returns None for subgenres, and a hex code like "#00aaff" for genres"""
-		return loads((await redis.hget(Subgenre._get_redis_key(genre_name), "color")).decode("utf8"))
+		return loads((await do_redis("hget", Subgenre._get_redis_key(genre_name), "color")).decode("utf8"))
 	
 	async def resolve_hex(self, info):
 		return await Color._get_hex_color(self.name)
 
 
 class Subgenre(ObjectType):
+	"""A subgenre, as understood on the Genre Sheet"""
+	
 	name = String(required=True)
 	is_genre = Boolean(required=True)
 	genre = Field(lambda: Subgenre, required=True)
@@ -61,14 +66,14 @@ class Subgenre(ObjectType):
 		return Subgenre._get_redis_key(self.name)
 	
 	async def resolve_is_genre(self, info):
-		result: Optional[str] = await redis.hget(self._redis_key, "is_genre")
+		result: Optional[str] = await do_redis("hget", self._redis_key, "is_genre")
 		
 		# Implied returning None for None (though that shouldn't happen?)
 		if result is not None:
 			return loads(result)
 	
 	async def resolve_genre(self, info):
-		return Subgenre(name=(await redis.hget(self._redis_key, "genre")).decode("utf8"))
+		return Subgenre(name=(await do_redis("hget", self._redis_key, "genre")).decode("utf8"))
 	
 	async def resolve_color(self, info):
 		genre = await self.resolve_genre(info)
@@ -76,27 +81,67 @@ class Subgenre(ObjectType):
 		return Color(name=name)
 	
 	async def resolve_origins(self, info):
-		return [Subgenre(name=name) for name in loads(await redis.hget(self._redis_key, "origins"))]
+		return [Subgenre(name=name) for name in loads(await do_redis("hget", self._redis_key, "origins"))]
 	
 	async def resolve_subgenres(self, info):
-		return [Subgenre(name=name) for name in loads(await redis.hget(self._redis_key, "subgenres"))]
+		return [Subgenre(name=name) for name in loads(await do_redis("hget", self._redis_key, "subgenres"))]
 
 
 class Query(ObjectType):
-	all_subgenres = List(Subgenre)
+	all_subgenres = List(Subgenre, description="Retrieve all subgenres from the database")
+	subgenre = Field(Subgenre(), name=String(), description="Retrieve a particular subgenre from the database")
 	
 	async def resolve_all_subgenres(self, info):
 		subgenres = []
-		for subgenre in await redis.smembers("subgenres"):
+		for subgenre in await do_redis("smembers", "subgenres"):
 			subgenres.append(Subgenre(name=subgenre.decode("utf8")))
 		
 		return subgenres
+	
+	async def resolve_subgenre(self, info, name):
+		return Subgenre(name=name)
+
+
+async def do_redis(command_name: str, *args, **kwds):
+	global actions
+	actions += 1
+	
+	return await getattr(redis_transaction, command_name)(*args, **kwds)
+
+
+async def supervise_redis_transactions():
+	global actions
+	global redis_transaction
+	
+	DIVISIONS_PER_GROUP: int = 4
+	SECONDS_PER_GROUP: float = 1 / 4
+	ACTIONS_THRESHOLD: int = 10
+	
+	while True:
+		for _ in range(DIVISIONS_PER_GROUP):
+			if actions >= ACTIONS_THRESHOLD:
+				print(f"{actions} actions exceeds threshold {ACTIONS_THRESHOLD}, executing right now", flush=True, file=stderr)
+				await redis_transaction.execute()
+				redis_transaction = redis.multi_exec()
+				actions = 0
+				break
+			
+			await sleep(SECONDS_PER_GROUP / DIVISIONS_PER_GROUP)
+		else:
+			if actions > 0:
+				print(f"only {actions} actions is not enough to meet {ACTIONS_THRESHOLD}, executing soon", flush=True, file=stderr)
+				loop.create_task(redis_transaction.execute())
+				redis_transaction = redis.multi_exec()
+				actions = 0
 
 
 app.add_route('/graphql', GraphQLApp(schema=Schema(query=Query, auto_camelcase=False), executor=AsyncioExecutor(loop=loop)))
 
 if __name__ == '__main__':
-	# Yes, we are really using a global redis connection for all queries, without authentication or rate-limiting
+	# Yes, we are really using a global Redis connection for all queries, without authentication or rate-limiting
 	redis: Redis = loop.run_until_complete(create_redis_pool("redis://redis", password=getenv("REDIS_PASSWORD")))
+	redis_transaction = redis.multi_exec()
+	actions = 0
+	loop.create_task(supervise_redis_transactions())
 	with closing(redis):
 		run(app, host='0.0.0.0', port=80, loop=loop)
