57ebce5a7b2d75d3a7af8e74165ddb80c6a1acfb
Navith <navithnavithnavith+git@gmail.com>
2019-01-02 22:35:48 -0500

    split up GraphQL server into multiple files

---
+++ api/graphql-server/subgenres.py
@@ -0,0 +1,115 @@
+from functools import lru_cache
+from json import loads
+from re import sub
+from typing import List as typing_List, Optional, cast
+
+from graphene import Boolean, Enum, Field, List, ObjectType, String
+from graphql import GraphQLError
+
+from .__main__ import do_redis
+
+
+class ColorRepresentation(Enum):
+	"""A format that a color can come in, such as HEX"""
+	
+	HEX = 0
+	TAILWIND = 1
+	
+	@property
+	def description(self):
+		if self == ColorRepresentation.HEX:
+			return 'The color as a hex code in a string, such as "#ec00db"'
+		elif self == ColorRepresentation.TAILWIND:
+			return 'The color as a TailwindCSS color name (as found in a class name), such as "genre-ambient"'
+
+
+class Color(ObjectType):
+	"""A color for genres and subgenres. Includes information about the name (which genre it's tied to) and a hex representation."""
+	
+	from_genre = Field(String, description="The name of the genre that this color comes from")
+	foreground = Field(String, representation=ColorRepresentation(name="representation", description="The format for the color, such as HEX"), description="The text color that appears on top of this color on the Genre Sheet")
+	background = Field(String, representation=ColorRepresentation(name="representation", description="The format for the color, such as HEX"), description="The background color for the genre on the Genre Sheet")
+	
+	@staticmethod
+	async def _get_hex_colors(genre_name: str) -> Optional[typing_List[str]]:
+		"""Returns None for subgenres, and a list of hex codes like ["#ec00db", "#ffffff"] for genres"""
+		return loads((await do_redis("hget", Subgenre._get_redis_key(genre_name), "color")).decode("utf8"))
+	
+	async def resolve_foreground(self, info, representation: ColorRepresentation = ColorRepresentation.HEX):
+		if representation == ColorRepresentation.HEX:
+			return (await Color._get_hex_colors(cast(str, self.from_genre)))[1]
+		elif representation == ColorRepresentation.TAILWIND:
+			foreground = (await Color._get_hex_colors(cast(str, self.from_genre)))[1].lower()
+			
+			# These are the only colors we use, so why bother with anything else?
+			if foreground == "#ffffff":
+				return "white"
+			elif foreground == "#000000":
+				return "black"
+			
+			raise GraphQLError(f"internal error: the foreground color represented by hex {foreground} is neither white nor black")
+		
+		raise GraphQLError(f"unknown representation {representation!r} for foreground color")
+	
+	@staticmethod
+	@lru_cache(maxsize=64)
+	def _create_class_name_for_genre(genre_name: str) -> str:
+		words: typing_List[str] = (cast(str, genre_name)).lower().split()
+		words_alpha_only: typing_List[str] = ["".join([letter for letter in word if letter.isalpha()]) for word in words]
+		words_hyphenated: str = '-'.join(words_alpha_only)
+		reduced_hyphens: str = sub(r"-+", "-", words_hyphenated)
+		return f"genre-{reduced_hyphens}"
+	
+	async def resolve_background(self, info, representation: ColorRepresentation = ColorRepresentation.HEX):
+		if representation == ColorRepresentation.HEX:
+			return (await Color._get_hex_colors(cast(str, self.from_genre)))[0]
+		elif representation == ColorRepresentation.TAILWIND:
+			return Color._create_class_name_for_genre(cast(str, self.from_genre))
+		
+		raise GraphQLError(f"unknown representation {representation!r} for background color")
+
+
+class Subgenre(ObjectType):
+	"""A subgenre, as understood on the Genre Sheet"""
+	
+	name = String(required=True, description='The name of the subgenre, e.x. "Brostep"')
+	is_genre = Boolean(required=True, description='Whether the subgenre is actually a genre (has its own category and color), e.x. true for Rock, false for Future House')
+	genre = Field(lambda: Subgenre, required=True, description="The category that this subgenre belongs to, and that it inherits its color from, e.x. Vaporwave for Vaportrap, Future Bass for Future Bass")
+	color = Field(Color, description="Color information about this subgenre, including the text color it uses on the Genre Sheet and the background color")
+	origins = List(lambda: Subgenre, required=True, description='The list of subgenres that this subgenre comes directly from, e.x. {Detroit Techno,} for Big Room Techno, {UK Hip Hop, 2-Step Garage} for Grime')
+	subgenres = List(lambda: Subgenre, required=True, description='The list of subgenres who originate directly from this subgenre, e.x. {Deathstep, Drumstep} for Dubstep, {} for Footwork, {Electro Swing, Jazzstep} for Nu-Jazz')
+	
+	@staticmethod
+	@lru_cache(maxsize=1024)
+	def _get_redis_key(subgenre_name: str) -> str:
+		return f"subgenre:{subgenre_name}"
+	
+	@staticmethod
+	@lru_cache(maxsize=64)
+	def _transform_unknown_subgenre(unknown_subgenre_text: str) -> str:
+		# ? (Pop) -> Pop
+		# Yes, I realize it's silly to reduce an unknown subgenre of a genre to that genre itself,
+		# But call me when it really is a problem
+		*_, genre_with_parenthesis = unknown_subgenre_text.partition("(")
+		return genre_with_parenthesis[:-1]
+	
+	@property
+	def _redis_key(self):
+		return Subgenre._get_redis_key(cast(str, self.name))
+	
+	async def resolve_is_genre(self, info):
+		result: Optional[str] = await do_redis("hget", self._redis_key, "is_genre")
+		return loads(result)
+	
+	async def resolve_genre(self, info):
+		return Subgenre(name=(await do_redis("hget", self._redis_key, "genre")).decode("utf8"))
+	
+	async def resolve_color(self, info):
+		genre = await self.resolve_genre(info)
+		return Color(from_genre=genre.name)
+	
+	async def resolve_origins(self, info):
+		return [Subgenre(name=name) for name in loads(await do_redis("hget", self._redis_key, "origins"))]
+	
+	async def resolve_subgenres(self, info):
+		return [Subgenre(name=name) for name in loads(await do_redis("hget", self._redis_key, "subgenres"))]
---
+++ api/graphql-server/tracks.py
@@ -0,0 +1,116 @@
+from datetime import date, datetime, timedelta
+from itertools import count
+from json import dumps, loads
+from typing import Generator, List as typing_List, Optional, Tuple, cast
+
+from graphene import Argument, Date, Field, ID, Int, ObjectType, String
+
+from api.genre_utils import flatten_subgenres
+from .__main__ import do_redis
+from .subgenres import ColorRepresentation, Subgenre
+
+
+class ReleaseDate(ObjectType):
+	datetime = Field(Date, name="datetime", description="(internal) datetime object used to determine the other fields")
+	
+	iso8601 = Field(Date, name="iso8601", description='The date as an ISO 8601 formatted string, like 2018-04-20')
+	year = Field(Int, name="year", description="The year the track released")
+	month_name = Field(String, name="month_name", description='The month the track released, as text ("December")')
+	month_int = Field(Int, name="month_int", description='The month the track released, as an integer (12)')
+	day = Field(Int, name="day", description="The day of the month the track released")
+	
+	def resolve_iso8601(self, info):
+		return cast(datetime, self.datetime).strftime("%Y-%m-%d")
+	
+	def resolve_year(self, info):
+		return cast(datetime, self.datetime).year
+	
+	def resolve_month_name(self, info):
+		return cast(datetime, self.datetime).strftime("%B")
+	
+	def resolve_month_int(self, info):
+		return cast(datetime, self.datetime).month
+	
+	def resolve_day(self, info):
+		return cast(datetime, self.datetime).day
+
+
+class Track(ObjectType):
+	id = Field(ID, name="id", description="The unique ID describing this track")
+	
+	name = Field(String, name="name", description="The name of the track")
+	
+	artist = Field(String, name="artist", description="The artist(s) of the track")
+	record_label = Field(String, name="record_label", description="The record label(s) who released and/or own the rights to the track")
+	date = Field(ReleaseDate, description="The release date of the track")
+	
+	subgenres_json = String(name="subgenres_json", description="The parsed list of subgenres that make up this song as a JSON string (really low level and provisional)")
+	subgenres_flat_json = String(name="subgenres_flat_json", and_colors=Argument(ColorRepresentation, required=False, description="Include a list of colors in sync with the list of subgenres with this specified representation (see ColorRepresentation for valid options)"),
+	                             description="The subgenres that make up this song, as a flat list (contained in a JSON string). Note that this introduces ambiguity with subgenre grouping")
+	
+	@staticmethod
+	def _get_redis_key(track_id: str) -> str:
+		return f"track:{track_id}"
+	
+	@property
+	def _redis_key(self):
+		return Track._get_redis_key(cast(str, self.id))
+	
+	async def resolve_name(self, info):
+		return (await do_redis("hget", self._redis_key, "track")).decode("utf8")
+	
+	async def resolve_artist(self, info):
+		return (await do_redis("hget", self._redis_key, "artist")).decode("utf8")
+	
+	async def resolve_record_label(self, info):
+		return (await do_redis("hget", self._redis_key, "label")).decode("utf8")
+	
+	async def resolve_date(self, info):
+		underlying_datetime: datetime = datetime.strptime((await do_redis("hget", self._redis_key, "release")).decode("utf8"), "%Y-%m-%d")
+		return ReleaseDate(datetime=underlying_datetime)
+	
+	async def resolve_subgenres_json(self, info):
+		return (await do_redis("hget", self._redis_key, "subgenre")).decode("utf8")
+	
+	async def resolve_subgenres_flat_json(self, info, and_colors: Optional[ColorRepresentation] = None):
+		flat_list: typing_List[str] = flatten_subgenres(loads(await self.resolve_subgenres_json(info)))
+		
+		if and_colors is None:
+			return dumps(flat_list)
+		
+		colors: typing_List[Optional[Tuple[str, str]]] = []
+		for subgenre in flat_list:
+			if subgenre in {"|", ">", "~"}:
+				colors.append(None)
+			else:
+				if await do_redis("sismember", "subgenres", subgenre):
+					color_info = await Subgenre(name=subgenre).resolve_color(info)
+					colors.append((await color_info.resolve_background(info, representation=and_colors), await color_info.resolve_foreground(info, representation=and_colors)))
+				else:
+					if not subgenre.startswith("?"):
+						# The subgenre does not exist
+						colors.append(("black", "white") if and_colors == ColorRepresentation.TAILWIND else ("#000000", "#ffffff"))
+					else:
+						subgenre = Subgenre._transform_unknown_subgenre(subgenre)
+						if await do_redis("sismember", "subgenres", subgenre):
+							color_info = await Subgenre(name=subgenre).resolve_color(info)
+							colors.append((await color_info.resolve_background(info, representation=and_colors), await color_info.resolve_foreground(info, representation=and_colors)))
+						else:
+							# The genre this is an unknown subgenre of does not exist
+							colors.append(("black", "white") if and_colors == ColorRepresentation.TAILWIND else ("#000000", "#ffffff"))
+		
+		return dumps([flat_list, colors])
+
+
+def all_dates_between(start: date, end: date, reverse: bool = False) -> Generator[date, None, None]:
+	range_ = range((end - start).days + 1)
+	if reverse:
+		range_ = reversed(range_)
+	
+	for i in range_:
+		yield start + timedelta(days=i)
+
+
+def all_dates_before(end: date) -> Generator[date, None, None]:
+	for i in count(0):
+		yield end - timedelta(days=i)
