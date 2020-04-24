725a2ea6573ce8f0c7c80fb4e40c5be48b9d3452
Navith <navithnavithnavith+git@gmail.com>
2020-04-24 04:36:50 -0400

    inconsequential whitespace changes

--- backend/sheet_to_db/__main__.py
+++ backend/sheet_to_db/__main__.py
@@ -14,6 +14,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+
 from asyncio import as_completed, run, wait_for
 from contextlib import closing
 from itertools import chain
--- frontend/src/components/Renderless/LocalStorage.svelte
+++ frontend/src/components/Renderless/LocalStorage.svelte
@@ -17,7 +17,6 @@
 -->
 
 
-
 <script lang="typescript">
 	import localStore from "./local-store";
 
